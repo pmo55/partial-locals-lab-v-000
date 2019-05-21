1604c7dc56e30ce409d82dc0cc7c59bbfdfeef4a
Parsa Moshiri <parsamoshiri5@gmail.com>
2019-05-21 21:31:39 +0000

    Automatically backed up by Learn

--- app/models/student.rb
+++ app/models/student.rb
@@ -16,7 +16,7 @@ class Student < ActiveRecord::Base
   
   def self.search(search)
     if search.present?
-      where('search LIKE ?',"%#{search}%")
+      where('name LIKE ?',"%#{search}%")
     else
      self.all
    end
